d443414c6d88f7ed1c235739d088b90509c84e35
Duc-Tam-Huynh <huynhductam96@gmail.com>
2026-09-16 22:11:08 +0700

    add morgan logger

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
-app.get('/tin-tuc', (req, res) => {
+app.use(morgan('combined'));
+
+
+app.get('/', (req, res) => {
 
   return res.send('Hello World!');
 });
